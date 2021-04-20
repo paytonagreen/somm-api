3d7980633f5a5d20ce671e321aa1d96ab9911ad6
Payton Green <paytonagreen@gmail.com>
2021-04-19 23:52:19 -0500

    migration prep

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@ protein1 = Protein.create(protein_name: "Beef")
 protein2 = Protein.create(protein_name: "Chicken")
 protein3 = Protein.create(protein_name: "Pork")
 
-wine1 = Wine.create(wine_name: "Cabernet Sauvignon", wine_description: "V good")
-wine2 = Wine.create(wine_name: "Chardonnay", wine_description: "V good")
-wine3 = Wine.create(wine_name: "Riesling", wine_description: "V good")
+wine1 = Wine.create(wine_name: "Cabernet Sauvignon", wine_description: "An absolute classic")
+wine2 = Wine.create(wine_name: "Chardonnay", wine_description: "Ooooh buttery")
+wine3 = Wine.create(wine_name: "Riesling", wine_description: "Sharply delicious")
 
 wine1.proteins << Protein.find(1)
